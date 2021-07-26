668011a5e07102cff9a3779810f956a4fd47ef49
cinthia <gcinthia919@gmail.com>
2021-07-26 18:09:08 -0300

    cree registros

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const app = express () ;
 const path = require ("path") ;
 const port = 3030;
 
+app.use(express.static('public'));
+
+app.get ("/", (req,res) => res.sendFile(path.join(__dirname, "views","index.html")))
+
+app.get ("/babbage", (req,res) => res.sendFile(path.join(__dirname, "views","babbage.html")))
+
+app.get ("/berners", (req,res) => res.sendFile(path.join(__dirname, "views","berners.html")))
+
+app.get ("/clarke", (req,res) => res.sendFile(path.join(__dirname, "views","clarke.html")))
+
+app.get ("/hamilton", (req,res) => res.sendFile(path.join(__dirname, "views","hamilton.html")))
+
+app.get ("/lovelace", (req,res) => res.sendFile(path.join(__dirname, "views","lovelace.html")))
+
+app.get ("/turing", (req,res) => res.sendFile(path.join(__dirname, "views","turing.html")))
 
 
 
